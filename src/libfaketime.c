oldbai555/libfaketime

commit 28f2707a550cf2ff3afc405ea138912b90cae937

Preserve positive wait timeouts

src/libfaketime.c
@@ -558,6 +558,20 @@ static double user_rate = 1.0;
 static bool user_rate_set = false;
 static struct timespec user_per_tick_inc = {0, -1};
 static bool user_per_tick_inc_set = false;
+
+static int scale_timeout_milliseconds(int timeout)
+{
+  long double scaled_timeout;
+
+  if (timeout <= 0 || !user_rate_set || dont_fake)
+    return timeout;
+  scaled_timeout = ceill((long double)timeout / (long double)user_rate);
+  if (scaled_timeout >= (long double)INT_MAX)
+    return INT_MAX;
+  if (scaled_timeout < 1.0L)
+    return 1;
+  return (int)scaled_timeout;
+}
 enum ft_mode_t {FT_FREEZE, FT_START_AT, FT_NOOP} ft_mode = FT_FREEZE;
 
 /* Time to fake is not provided through FAKETIME env. var. */
@@ -2200,14 +2214,7 @@ int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
   {
     return -1;
   }
-  if (user_rate_set && !dont_fake && timeout > 0)
-  {
-    real_timeout = (int) timeout * 1.0/user_rate;
-  }
-  else
-  {
-    real_timeout = timeout;
-  }
+  real_timeout = scale_timeout_milliseconds(timeout);
   DONT_FAKE_TIME(ret = (*real_epoll_wait)(epfd, events, maxevents, real_timeout));
   return ret;
 }
@@ -2224,14 +2231,7 @@ int epoll_pwait(int epfd, struct epoll_event *events, int maxevents, int timeout
   {
     return -1;
   }
-  if (user_rate_set && !dont_fake && timeout > 0)
-  {
-    real_timeout = (int) timeout * 1.0/user_rate;
-  }
-  else
-  {
-    real_timeout = timeout;
-  }
+  real_timeout = scale_timeout_milliseconds(timeout);
   DONT_FAKE_TIME(ret = (*real_epoll_pwait)(epfd, events, maxevents, real_timeout, sigmask));
   return ret;
 }
@@ -2246,13 +2246,15 @@ int macos_poll(struct pollfd *fds, nfds_t nfds, int timeout)
 int poll(struct pollfd *fds, nfds_t nfds, int timeout)
 #endif
 {
-  int ret, timeout_real = (user_rate_set && !dont_fake && (timeout > 0))?(timeout / user_rate):timeout;
+  int ret;
+  int timeout_real;
 
   ftpl_init();
   if (real_poll == NULL)
   {
     return -1;
   }
+  timeout_real = scale_timeout_milliseconds(timeout);
 
 #ifdef MACOS_DYLD_INTERPOSE
   DONT_FAKE_TIME(ret = (*poll)(fds, nfds, timeout_real));

test/Makefile
@@ -9,7 +9,7 @@ OBJ = ${SRC:.c=.o}
 TEST_SNIPPETS = $(notdir $(basename $(wildcard snippets/*.c)))
 EXPECTATIONS = $(notdir $(basename $(wildcard snippets/*.variable)))
 
-ALL_TESTS = timetest test shm_layout_test set_mtime time_ops_test clock_error_test wait_contract_test sem_contract_test timerfd_contract_test
+ALL_TESTS = timetest test shm_layout_test set_mtime time_ops_test clock_error_test wait_contract_test sem_contract_test wait_timeout_contract_test timerfd_contract_test
 
 ifneq ($(filter -DINTERCEPT_SYSCALL,${CFLAGS}),)
 ALL_TESTS += confirm_variadic_promotion syscall_contract_test
@@ -63,6 +63,9 @@ timerfd_contract_test: timerfd_contract_test.c
 sem_contract_test: sem_contract_test.c
 	${CC} -o $@ ${CFLAGS} $< ${LDFLAGS}
 
+wait_timeout_contract_test: wait_timeout_contract_test.c
+	${CC} -o $@ ${CFLAGS} $< ${LDFLAGS}
+
 init_reentry.so: init_reentry.c
 	${CC} -shared -fPIC -o $@ ${CFLAGS} $<
 
@@ -81,7 +84,7 @@ file_timestamp_test: file_timestamp_test.c
 syscall_contract_test: syscall_contract_test.c
 	${CC} -o $@ ${CFLAGS} $<
 
-test: timetest time_ops_test set_mtime clock_error_test wait_contract_test sem_contract_test timerfd_contract_test init_reentry.so ${RANDOM_TEST_DEP} ${FILE_TIMESTAMP_TEST_DEP} ${FAKE_PID_TEST_DEP} ${SYSCALL_TEST_DEP} functest
+test: timetest time_ops_test set_mtime clock_error_test wait_contract_test sem_contract_test wait_timeout_contract_test timerfd_contract_test init_reentry.so ${RANDOM_TEST_DEP} ${FILE_TIMESTAMP_TEST_DEP} ${FAKE_PID_TEST_DEP} ${SYSCALL_TEST_DEP} functest
 	@echo
 	@./time_ops_test
 	@./test.sh
@@ -129,7 +132,7 @@ use_lib_%: _use_lib_test.c snippets/%.c lib%.so
 ## cleanup and metainformation
 
 clean:
-	@rm -f ${OBJ} timetest shm_layout_test set_mtime time_ops_test clock_error_test wait_contract_test sem_contract_test timerfd_contract_test init_reentry.so fake_pid_test fake_pid_contract_test random_test file_timestamp_test syscall_contract_test getrandom_test syscall_test $(foreach f,${TEST_SNIPPETS},use_lib_${f} lib${f}.so run_${f}) variadic_promotion variadic/*.o repeat_random libmallocintercept.so
+	@rm -f ${OBJ} timetest shm_layout_test set_mtime time_ops_test clock_error_test wait_contract_test sem_contract_test wait_timeout_contract_test timerfd_contract_test init_reentry.so fake_pid_test fake_pid_contract_test random_test file_timestamp_test syscall_contract_test getrandom_test syscall_test $(foreach f,${TEST_SNIPPETS},use_lib_${f} lib${f}.so run_${f}) variadic_promotion variadic/*.o repeat_random libmallocintercept.so
 
 distclean: clean
 	@echo

test/Makefile.OSX
@@ -13,7 +13,7 @@ ifneq ($(filter -DFAKE_PID,${CFLAGS}),)
 FAKE_PID_TEST_DEP = fake_pid_contract_test
 endif
 
-all: timetest set_mtime time_ops_test macos_clock_test clock_error_test wait_contract_test sem_contract_test init_reentry.dylib ${FILE_TIMESTAMP_TEST_DEP} ${FAKE_PID_TEST_DEP} test
+all: timetest set_mtime time_ops_test macos_clock_test clock_error_test wait_contract_test sem_contract_test wait_timeout_contract_test init_reentry.dylib ${FILE_TIMESTAMP_TEST_DEP} ${FAKE_PID_TEST_DEP} test
 
 .c.o:
 	${CC} -c ${CFLAGS} $<
@@ -36,6 +36,9 @@ wait_contract_test: wait_contract_test.c
 sem_contract_test: sem_contract_test.c
 	${CC} -o $@ ${CFLAGS} $<
 
+wait_timeout_contract_test: wait_timeout_contract_test.c
+	${CC} -o $@ ${CFLAGS} $<
+
 init_reentry.dylib: init_reentry.c
 	${CC} -dynamiclib -fPIC -o $@ ${CFLAGS} $<
 
@@ -48,7 +51,7 @@ fake_pid_contract_test: fake_pid_contract_test.c
 macos_clock_test: macos_clock_test.c
 	${CC} -o $@ ${CFLAGS} $< ../src/libfaketime.1.dylib
 
-test: timetest time_ops_test set_mtime macos_clock_test clock_error_test wait_contract_test sem_contract_test init_reentry.dylib ${RANDOM_TEST_DEP} ${FILE_TIMESTAMP_TEST_DEP} ${FAKE_PID_TEST_DEP} functest
+test: timetest time_ops_test set_mtime macos_clock_test clock_error_test wait_contract_test sem_contract_test wait_timeout_contract_test init_reentry.dylib ${RANDOM_TEST_DEP} ${FILE_TIMESTAMP_TEST_DEP} ${FAKE_PID_TEST_DEP} functest
 	@echo
 	@./time_ops_test
 	@./test_OSX.sh
@@ -58,7 +61,7 @@ functest:
 	./testframe.sh functests
 
 clean:
-	@rm -f ${OBJ} timetest set_mtime time_ops_test macos_clock_test clock_error_test wait_contract_test sem_contract_test init_reentry.dylib file_timestamp_test fake_pid_contract_test
+	@rm -f ${OBJ} timetest set_mtime time_ops_test macos_clock_test clock_error_test wait_contract_test sem_contract_test wait_timeout_contract_test init_reentry.dylib file_timestamp_test fake_pid_contract_test
 
 distclean: clean
 	@echo

test/functests/test_clock_contract.sh
@@ -23,6 +23,15 @@ run()
 	run_testcase null_clock_output_fails
 	run_testcase past_wait_deadlines_timeout
 	run_testcase semaphore_deadlines
+	run_testcase positive_poll_timeout
+}
+
+positive_poll_timeout()
+{
+	typeset result
+	result=$(fakecmd "+0 x2" ./wait_timeout_contract_test)
+	asserteq "$result" "positive poll timeout was preserved" \
+		"positive poll timeout should not be truncated to zero"
 }
 
 semaphore_deadlines()

test/wait_timeout_contract_test.c
@@ -1,0 +1,40 @@
+#include <errno.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+static long long nanoseconds(const struct timespec *value)
+{
+  return (long long)value->tv_sec * 1000000000LL + value->tv_nsec;
+}
+
+int main(void)
+{
+  struct timespec before;
+  struct timespec after;
+  int result;
+  long long elapsed;
+
+  if (setenv("FAKETIME_DONT_FAKE_MONOTONIC", "1", 1) == -1 ||
+      clock_gettime(CLOCK_MONOTONIC_RAW, &before) == -1)
+  {
+    perror("clock setup");
+    return EXIT_FAILURE;
+  }
+  errno = 0;
+  result = poll(NULL, 0, 20);
+  if (result != 0 || errno != 0 || clock_gettime(CLOCK_MONOTONIC_RAW, &after) == -1)
+  {
+    perror("poll");
+    return EXIT_FAILURE;
+  }
+  elapsed = nanoseconds(&after) - nanoseconds(&before);
+  if (elapsed < 5000000LL || elapsed > 500000000LL)
+  {
+    fprintf(stderr, "poll elapsed %lld ns outside expected range\n", elapsed);
+    return EXIT_FAILURE;
+  }
+  puts("positive poll timeout was preserved");
+  return EXIT_SUCCESS;
+}

commit 3a3663f729c4447618e259aba89f4d6e078cfcbf

Guard scale_timeout_milliseconds with FAKE_SLEEP
scale_timeout_milliseconds() is defined unconditionally, but its only
callers are the poll(), epoll_wait() and epoll_pwait() wrappers, which
are all compiled under #ifdef FAKE_SLEEP. Building without FAKE_SLEEP
therefore fails, as warnings are treated as errors:

    $ FAKETIME_COMPILE_CFLAGS="-UFAKE_STAT -UFAKE_UTIME -UFAKE_SLEEP" make
    make  -C src all
    make[1]: Entering directory '/home/user/libfaketime/src'
    cc -o libfaketime.o -c -std=gnu99 -Wall -Wextra -Werror -DFAKE_PTHREAD -DFAKE_STAT -DFAKE_UTIME -DFAKE_SLEEP -DFAKE_TIMERS -DFAKE_INTERNAL_CALLS -fPIC -DPREFIX='"'/usr/local'"' -DLIBDIRNAME='"'/lib/faketime'"' -UFAKE_STAT -UFAKE_UTIME -UFAKE_SLEEP -Wno-nonnull-compare   libfaketime.c
    libfaketime.c:682:12: error: 'scale_timeout_milliseconds' defined but not used [-Werror=unused-function]
      682 | static int scale_timeout_milliseconds(int timeout)
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~
    cc1: all warnings being treated as errors
    make[1]: *** [Makefile:207: libfaketime.o] Error 1

This is the build configuration used by python-libfaketime, which
compiles the vendored library on the user's machine at install time.
The CI matrix only ever adds -D flags, so the regression introduced in
28f2707 went unnoticed.

Guarding the definition leaves the default build unchanged.

src/libfaketime.c
@@ -679,6 +679,7 @@ static bool user_rate_set = false;
 static struct timespec user_per_tick_inc = {0, -1};
 static bool user_per_tick_inc_set = false;
 
+#ifdef FAKE_SLEEP
 static int scale_timeout_milliseconds(int timeout)
 {
   long double scaled_timeout;
@@ -692,6 +693,7 @@ static int scale_timeout_milliseconds(int timeout)
     return 1;
   return (int)scaled_timeout;
 }
+#endif
 enum ft_mode_t {FT_FREEZE, FT_START_AT, FT_NOOP} ft_mode = FT_FREEZE;
 
 /* Time to fake is not provided through FAKETIME env. var. */